af6191b800574da6bd29e6736670d7f36c6a2306
Kevdevui <kevinajcabey@hotmail.co.uk>
2024-08-01 20:02:29 +0100

    Second commit - completed 2 to 4 modules

---
+++ module2.py
@@ -0,0 +1,13 @@
+# Car Price Checker
+# product prices
+ferrari = 420000
+toyota = 21000
+# which product cost less
+if ferrari < toyota:
+    print("ferrari is less than toyota.")
+elif toyota < ferrari:
+    print("toyota is less than ferrari.")
+else:
+    print("Both cars have the same price.")
+    
+print(3 * 'abc')
---
+++ module3.py
@@ -0,0 +1,15 @@
+# Variables for personal information
+name = "Kevin"
+age = 32
+city = "Manchester"
+hobby = "Gaming"
+
+# Show personal information
+print(">--------------------------------<")
+print("       Personal Information")
+print(">--------------------------------<")
+print("Name:  ", name)
+print("Age:   ", age)
+print("City:  ", city)
+print("Hobby: ", hobby)
+print(">--------------------------------<")
